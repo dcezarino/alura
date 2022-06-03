a013495baa4a6a932c3bf3c1963d3911a116ce2c
Diego Cezarino <cezarino.diego@gmail.com>
2022-06-02 23:03:44 -0300

    Removendo comentários

--- kafka_projeto/ecommerce/src/test/java/br/com/alura/ecommerce/NewOrderMain.java
+++ kafka_projeto/ecommerce/src/test/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -7,23 +7,15 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
        try (var dispatcher = new KafkaDispatcher()) {
-        for (int i = 0; i < 10; i++) {
-            var key = UUID.randomUUID().toString();
-            var value = key + ", 33333, 44444";
-            dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+           for (int i = 0; i < 10; i++) {
+               var key = UUID.randomUUID().toString();
+               var value = key + ", 33333, 44444";
+               dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
 
-            /*
-             * Então com esse callback, eu preciso de um e-mail record aqui também,
-             * emailRecord, ProducerRecord e o tópico é e-ECOMMERCE_SEND_EMAIL,
-             * aqui eu estou pedindo realmente para enviar um e-mail.
-             */
-            var email = "Thank you for your order! We are processing your order!";
+               var email = "Thank you for your order! We are processing your order!";
+               dispatcher.send("ECOMMERCE_NEW_ORDER", key, email);
+           }
 
-            /*
-             * Passando o valor do e-mail, tanto a chave, quanto o valor.
-             */
-            dispatcher.send("ECOMMERCE_NEW_ORDER", key, email);
-        }
        }
     }
 }
