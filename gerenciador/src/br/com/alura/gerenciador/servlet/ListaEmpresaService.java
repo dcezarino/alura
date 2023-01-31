9607e844ae95f3e963cdd4b8d0c1b82e90378b82
Diego Cezarino <cezarino.diego@gmail.com>
2023-01-31 20:27:39 -0300

    Criando serviço para retornar o conteúdo da lista de empresas no formato xml ou json.

--- gerenciador/src/br/com/alura/gerenciador/servlet/ListaEmpresaService.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/ListaEmpresaService.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 @WebServlet("/empresas")
 public class ListaEmpresaService extends HttpServlet {
@@ -19,12 +20,35 @@ public class ListaEmpresaService extends HttpServlet {
 		
 		List<Empresa> listEmpresas = new Banco().getEmpresas();
 		
-		Gson gson = new Gson();
-		String json = gson.toJson(listEmpresas);
-				
-		response.setContentType("application/json");
-		response.getWriter().print(json);
+		// Ler cabeçalho da requisição
+		String valor = request.getHeader("Accept");
 		
+		System.out.println(valor);
+		
+		if (valor.contains("xml")) {
+			
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class);
+			String xml = xstream.toXML(listEmpresas);
+							
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+			
+		} else if (valor.endsWith("json"))  {
+			
+			// Biblioteca do Google (GSON)
+			Gson gson = new Gson();
+			String json = gson.toJson(listEmpresas);
+					
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+			
+		} else {
+			
+			response.setContentType("application/json");
+			response.getWriter().print("{'message:':'no content'}");
+			
+		}
 
 	}
 
